39df355fca02f6f7ed8b83a63082d290c19ba36e
Luisa Mazzi <luisa.c.mazzi@gmail.com>
2022-12-12 15:01:11 -0300

    Done

--- lib/main.dart
+++ lib/main.dart
@@ -8,25 +8,45 @@ void main() {
 class XylophoneApp extends StatelessWidget {
   const XylophoneApp({Key? key}) : super(key: key);
 
+  void playSound(int num) async {
+    final player = AudioPlayer();
+    await player.play(AssetSource('note$num.wav'));
+  }
+
+  Expanded buildKey(Color color, int num) {
+    return Expanded(
+      child: ElevatedButton(
+          onPressed: () {
+            playSound(num);
+          },
+          style: ButtonStyle(
+              backgroundColor:
+              MaterialStateProperty.all<Color>(color)),
+          child: null),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-              child: TextButton(
-                onPressed: () async {
-                  final player = AudioPlayer();
-                  await player.play(AssetSource('note1.wav'));
-                },
-                child: const Text('Click Me'),
-            ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(Colors.red, 1),
+              buildKey(Colors.orange, 2),
+              buildKey(Colors.yellow, 3),
+              buildKey(Colors.green, 4),
+              buildKey(Colors.teal, 5),
+              buildKey(Colors.blue,  6),
+              buildKey(Colors.purple, 7)
+             ],
           ),
         ),
       ),
     );
   }
 }
-
-
-
